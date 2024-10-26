5eb4f83e5923387b1379813fbe893333fdf9eade
daniel1876-zhuo <danielzhuo200793@163.com>
2024-10-27 02:27:24 +0800

    Update test.py
    added updating stats.txt when clicks know or not know the answer. 
    prepared the directory of the four files that may be needed.
    added a decide_next_question() which will use the function Evan writes.

--- test.py
+++ test.py
@@ -10,16 +10,15 @@ class RevisionPage(QWidget):
     A weight should be calculated for each flashcard. This weight determines how often the flashcard comes up,
     and should be calculated using user statistics (correct rate, time since last seen, etc.) stored in
     the flashcard statistics file ./current/stats.txt
+
     The file format for stats.txt is: one row for each flashcard, each row has 4 numbers separated by spaces.
     4 numbers are: Number of times shown, Number of correct answers, flashcards since last seen, and whether
     the last answer was correct or not.
+
     A random flashcard (determined by weight) should be repeatedly rendered, and the user can click to see the answer.
     The answer would be shown, and the user can select if they are correct or not. Relevant statistics should be updated
-    in stats.txt . Flashcard files are accessable in ./current/flashcards , and the pointer towards flashcard files is
-    ./current/metadata.txt , with file format as follows:
-    Row 1: name of flashcard
-    Subsequent rows: Question file name and answer file name, separated by a colon ':'
-    e.g. q1.txt:a1.txt
+    in stats.txt .
+
     The user can click a button to exit back to the flashcardmenu.
     """
 
@@ -57,20 +56,51 @@ class RevisionPage(QWidget):
         self.layout.addWidget(self.dont_know_button)
         self.layout.addWidget(back_flashcards_button)
         self.layout.addWidget(next_question_button)
-
         self.setLayout(self.layout)
+        self.question_id = 0
+        with open("./current/stats.txt","r",encoding="utf-8") as f:
+            self.stats = f.readlines()
 
 
     def know_answer(self):
         """Handle the case when the user knows the answer."""
         QMessageBox.information(self, "Correct!", "Great job! You knew the answer!")
-
+        #update stats.txt
+        tem = self.stats[self.question_id-1].split(' ')
+        self.stats[self.question_id-1] = f"{int(tem[0])+1} {int(tem[1])+1} {0} {True}\n"
+        with open("./current/stats.txt","w",encoding="utf-8") as f:
+            f.writelines(self.stats)
+        #next question
+        self.next_question()
 
     def dont_know_answer(self):
         """Handle the case when the user doesn't know the answer."""
         QMessageBox.information(self, "Keep Trying!", "No worries! Keep practicing.")
-
+        # update stats.txt
+        tem = self.stats[self.question_id - 1].split(' ')
+        self.stats[self.question_id - 1] = f"{int(tem[0]) + 1} {int(tem[1])} {0} {False}\n"
+        with open("./current/stats.txt", "w", encoding="utf-8") as f:
+            f.writelines(self.stats)
+        #next question
+        self.next_question()
 
     def next_question(self):
+        question_id = self.decide_next_question(self.stats)
         """Placeholder for showing the next question."""
-        QMessageBox.information(self, "Next Question", "Here would be the next question.")
+
+        directory = "./current/flashcards"
+        metadata = []
+        with open("./current/metadata.txt","r",encoding="utf-8") as f:
+            metadata = f.readlines()
+
+        #the locations of the four files
+        question_description_text = f"{directory}/{metadata[question_id * 2].split(':')[0]}"
+        question_file = f"{directory}/{metadata[question_id * 2 + 1].split(':')[0]}"
+        answer_description_text = f"{directory}/{metadata[question_id * 2].split(':')[1][:-1]}"
+        answer_file = f"{directory}/{metadata[question_id * 2 + 1].split(':')[1][:-1]}"
+
+        #QMessageBox.information(self, "Next Question", "Here would be the next question.")
+
+    def decide_next_question(self,stats): #return the id of next_question
+
+        pass
