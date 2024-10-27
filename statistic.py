b9cd3bb31249b99fab71194f1fd5c84b5f97f80f
daniel1876-zhuo <danielzhuo200793@163.com>
2024-10-28 00:13:13 +0800

    Update statistic.py
    correct the parameters in stats.

--- statistic.py
+++ statistic.py
@@ -1,3 +1,5 @@
+import os.path
+import shutil
 import sys
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout, QWidget, QFrame, QScrollArea, QHBoxLayout,
@@ -20,15 +22,14 @@ class StatisticsPage(QWidget):
     def __init__(self, switch_back_to_menu):
         super().__init__()
 
-        self.Layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
 
         # Title label
-        self.title = QLabel("Statistics")
-        self.Layout.addWidget(self.title)
+        self.layout.addWidget(QLabel("Statistics"))
 
         # List widget to display accuracy of each question
         self.frame = QFrame()
-        #accuracy_list is embedded in self.scroll_area, so no need to self.Layout.addWidget(self.accuracy_list)
+        #accuracy_list is embedded in self.scroll_area, so no need to self.layout.addWidget(self.accuracy_list)
         self.accuracy_list = QVBoxLayout(self.frame)
         # Sample data (to be replaced with actual data)
         #the format of self.stats: each question consists of onw row and two colomns. First column contains its id
@@ -63,10 +64,10 @@ class StatisticsPage(QWidget):
         self.wrap_sort_buttons = QWidget()
         self.wrap_sort_buttons.setLayout(self.sort_buttons)
         # Add widgets to layout
-        self.Layout.addWidget(back_menu_button)
-        self.Layout.addWidget(self.scroll_area)
-        self.Layout.addWidget(self.wrap_sort_buttons)
-        self.setLayout(self.Layout)
+        self.layout.addWidget(back_menu_button)
+        self.layout.addWidget(self.scroll_area)
+        self.layout.addWidget(self.wrap_sort_buttons)
+        self.setLayout(self.layout)
     #sort the stats list by these three options
     def sort_by_accuracy(self):
         self.stats = sorted(self.stats,key=lambda x:float( x[1].split(':')[1].split('%')[0][1:] ))
@@ -82,12 +83,10 @@ class StatisticsPage(QWidget):
 
     def obtain_stats(self): #obtain data from stats.txt, returns the list
         li = []
-        try:
-            with open("./current/stats.txt","r",encoding="utf-8") as f:
-                stats = f.readlines()
-        except:
-            print("exception: stats empty")
-            stats = []
+        if(os.path.isdir("./current") == False):
+            shutil.copytree("./New flashcards","./current")
+        with open("./current/stats.txt","r",encoding="utf-8") as f:
+            stats = f.readlines()
         for i in range(1,len(stats)+1):
             subli = []
             try:
@@ -96,14 +95,17 @@ class StatisticsPage(QWidget):
             except:
                 subli.append(f"Question {i}: Unknown: No such question exists.")
             try:
-                accuracy = round(100*(float(stats[i-1].split(' ')[0])) / (float(stats[i-1].split(' ')[1])),2)
+                accuracy = round(100*(float(stats[i-1].split(' ')[1])) / (float(stats[i-1].split(' ')[0])),2)
             except: ##division by zero
                 accuracy = 0
-            last_time_seen = stats[i-1].split(' ')[2]
-            number_of_time_seen = stats[i-1].split(' ')[1]
+            try:
+                last_time_seen = stats[i-1].split(' ')[2]
+                number_of_time_seen = stats[i-1].split(' ')[0]
+            except:
+                last_time_seen = 0
+                number_of_time_seen = 0
             subli.append(f"Accuracy: {accuracy}%.  Last Time Seen: {last_time_seen} flashcards ago.  Number Of Time: {number_of_time_seen}.\n")
             li.append(subli)
-            print(li,"test")
         return li
 
     def reset_accuracy_list(self):
@@ -120,84 +122,3 @@ class StatisticsPage(QWidget):
             self.accuracy_list.addWidget(content_label)
             content_label = QLabel(content[1])
             self.accuracy_list.addWidget(content_label)
-
-    def refreshpage(self,switch):
-        while self.layout().count():
-            content = self.layout().takeAt(0)
-            widget = content.widget()
-            if widget:
-                widget.deleteLater()
-            self.layout().removeItem(content)
-
-         # Title label
-        self.title = QLabel("Statistics")
-        self.Layout.addWidget(self.title)
-
-        # List widget to display accuracy of each question
-        self.frame = QFrame()
-        #accuracy_list is embedded in self.scroll_area, so no need to self.Layout.addWidget(self.accuracy_list)
-        self.accuracy_list = QVBoxLayout(self.frame)
-        # Sample data (to be replaced with actual data)
-        #the format of self.stats: each question consists of onw row and two colomns. First column contains its id
-        #and the question description Second row contains the accuracy, number of flashcards ago of its last time seen,
-        #and the total number of this flashcard seen
-        self.stats = self.obtain_stats()
-        self.reset_accuracy_list()
-
-        #make the accuracy list to be scrollable
-        self.scroll_area = QScrollArea()
-        self.scroll_area.setWidgetResizable(True)
-        self.wrap_accuracy_list = QWidget()
-        self.wrap_accuracy_list.setLayout(self.accuracy_list)
-        self.scroll_area.setWidget(self.wrap_accuracy_list)
-
-        #provide three ways to sort it
-        sort_by_accuracy = QPushButton("Sort By Accuracy")
-        sort_by_accuracy.clicked.connect(self.sort_by_accuracy)
-        sort_by_last_time_seen = QPushButton("Sort By Last Time Seen")
-        sort_by_last_time_seen.clicked.connect(self.sort_by_last_time_seen)
-        sort_by_number_of_time_seen = QPushButton("Sort By Total Time Seen")
-        sort_by_number_of_time_seen.clicked.connect(self.sort_by_number_of_time_seen)
-
-        # Button to go back to Menu page
-        back_menu_button = QPushButton("Back to Menu")
-        back_menu_button.clicked.connect(switch)
-        #create a horizontal box layout
-        self.sort_buttons = QHBoxLayout()
-        self.sort_buttons.addWidget(sort_by_accuracy)
-        self.sort_buttons.addWidget(sort_by_number_of_time_seen)
-        self.sort_buttons.addWidget(sort_by_last_time_seen)
-        self.wrap_sort_buttons = QWidget()
-        self.wrap_sort_buttons.setLayout(self.sort_buttons)
-        # Add widgets to layout
-        self.Layout.addWidget(back_menu_button)
-        self.Layout.addWidget(self.scroll_area)
-        self.Layout.addWidget(self.wrap_sort_buttons)
-
-        self.layout().update()
-        
-'''
-class statistics(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.setWindowTitle("Statistics")
-
-    def initUI(self):
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-        self.layout = QVBoxLayout(self.central_widget)
-        self.layout.addWidget(QLabel("Statistics"))
-
-    def use_store_accuracy(self):
-        return store_accuracy(self)
-
-    def use_next_question(self):
-        return next_question(self)
-
-def next_question(list,accuracy):
-    pass
-
-def store_accuracy(id,is_correct):
-    pass'''
-
