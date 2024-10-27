8e5e5b2074398f2efcf6ce22b1cfa1ab0d2e6aaa
daniel1876-zhuo <danielzhuo200793@163.com>
2024-10-28 00:31:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os.path
 import sys
-from zipfile import ZipFile
+from zipfile import ZipFile,ZIP_DEFLATED
+import pathlib
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout, QWidget,
     QPushButton, QLabel, QStackedWidget, QFileDialog, QMessageBox, QListWidget
@@ -23,13 +25,13 @@ class MainWindow(QMainWindow):
         # Create instances of pages
         self.menu_page = MenuPage(
             self.create_flashcards,
-            self.import_flashcards
+            self.import_flashcards,
+            self.show_statistics_page
         )
         self.flashcards_page = FlashcardsPage(
             self.show_add_flashcard_page,
             self.show_revision_page,
             self.show_menu_page,
-            self.show_statistics_page
         )
         self.add_flashcard_page = EditorPage(self.show_flashcards_page,self.refresheditor)
         
@@ -46,6 +48,7 @@ class MainWindow(QMainWindow):
         self.stacked_widget.addWidget(self.add_flashcard_page)
         self.stacked_widget.addWidget(self.revision_page)
         self.stacked_widget.addWidget(self.statistics_page)
+        # self.stacked_widget.addWidget(self.statistics_page)
 
         # Set the stacked widget as central widget
         self.setCentralWidget(self.stacked_widget)
@@ -87,8 +90,40 @@ class MainWindow(QMainWindow):
             print(e)
             QMessageBox.information(self,"","Flashcards failed to load")
 
+    def saveall(self): #save to ./stored
+        if(os.path.isdir("./stored") == False):
+            os.mkdir("./stored")
+        try:
+            with open("./current/metadata.txt","r",encoding="utf-8") as f:
+                target_name = f.readlines()[0][0:-1]
+        except:
+            target_name = "unnamed_folder"
+        target_path = "./stored/"
+        target_path_id = 1
+
+        if(os.path.exists(f"{target_path}{target_name}.zip") == True):
+            while True:
+                if os.path.exists(f"./stored/{target_name}_{target_path_id}.zip") == True:
+                    target_path_id += 1
+                else:
+                    break
+            target_name = f"{target_name}_{target_path_id}.zip"
+        else:
+            target_name = f"{target_name}.zip"
+
+        target_path = f"{target_path}{target_name}"
+        print(target_path,"is the location the zip stores")
+        print("is file already existed:",os.path.exists(target_path))
+        if os.path.isdir("./current"):
+            folder = pathlib.Path("./current")
+            with ZipFile(target_path,"w",ZIP_DEFLATED) as zip:
+                for file in folder.rglob('*'):
+                    print(file.name,"is being zipped as a part of the zip file")
+                    zip.write(file,file.relative_to(folder.parent))
+
     def show_menu_page(self):
         """Switch to the menu page."""
+        self.saveall()
         self.stacked_widget.setCurrentWidget(self.menu_page)
 
     def show_flashcards_page(self):
@@ -105,15 +140,12 @@ class MainWindow(QMainWindow):
         """Switch to the add flashcard page."""
         self.stacked_widget.setCurrentWidget(self.add_flashcard_page)
 
-
     def show_revision_page(self):
         """Switch to the revision page."""
         self.stacked_widget.setCurrentWidget(self.revision_page)
 
-
     def show_statistics_page(self):
         """Switch to the statistics page."""
-        self.statistics_page.refreshpage(self.show_flashcards_page)
         self.stacked_widget.setCurrentWidget(self.statistics_page)
 
 
