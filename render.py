367aa322c353e14f45b14d73066457a1b619a2dc
daniel1876-zhuo <danielzhuo200793@163.com>
2024-10-28 12:52:34 +0800

    Update render.py
    fix small bugs

--- render.py
+++ render.py
@@ -13,16 +13,19 @@ from moviepy.editor import VideoFileClip,AudioFileClip
 from PySide6.QtMultimedia import (
     QMediaFormat,QMediaPlayer,QAudioOutput,QSoundEffect
 )
-# f = QMediaFormat()
-# print(QMediaFormat.supportedFileFormats(f,QMediaFormat.Decode))
 #plan: after done, change to video part into a new class, leave the original video part with a button instead
-# which shows pop-out window of playing the video.
+#which shows pop-out window of playing the video.
+
+#a class that renders a file, given the question_id (so that the file location can be found by using metadata.txt)
 class MediaPlayer(QWidget):
+    #initialize the class, "answer" parameter means is finding question file or answer file
     def __init__(self,flashnum,answer=False):
         super().__init__()
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.renderflashcard(flashnum,answer)
+
+
     def renderfile(self,flashnum,answer=False):
         """
         Takes in flashcard number (starting from 1)
@@ -31,6 +34,7 @@ class MediaPlayer(QWidget):
         """
         with open("./current/metadata.txt", "r", encoding="utf-8") as f:
             try:
+                #self.fileloc is the file location found by reading metadata
                 if answer:
                     self.fileloc = "./current/flashcards/"+f.readlines()[flashnum+1][0:-1].split(":")[1]
                 else:
@@ -43,6 +47,7 @@ class MediaPlayer(QWidget):
 
                 return self.scroll
         try:
+            #extension is the file suffix (e.g. .png, .mp4, etc)
             extension = self.fileloc[-4:].lower()
             print("moved on",extension,self.fileloc)
             if extension == ".txt":
@@ -54,6 +59,7 @@ class MediaPlayer(QWidget):
                 self.scroll.setFixedHeight(300)
                 self.scroll.setWidget(self.label)
                 return self.scroll
+            #the file is an iamge, use Pixmap to render the file
             elif extension in (".gif,.jpg,.jpeg,.png"):
                 self.label = QLabel()
                 pixmap = QPixmap(self.fileloc)
@@ -64,24 +70,29 @@ class MediaPlayer(QWidget):
                 self.label.setPixmap(pixmap)
                 print("rendering image!")
                 return self.label
+            #the file is a video, use pygame to show it
             elif extension in (".mp4,.avi,.mov"): ##video
+                #create a "play video" button, when it is clicked it pops out a window that shows the video.
                 self.playing = QPushButton("Play The Video")
                 self.playing.setMinimumHeight(300)
                 self.playing.clicked.connect(self.playvideo)
                 return self.playing
+            #the file is an audio, use QMediaPlayer to display it
             elif extension in (".mp3, .wav"):
+                #create two buttons to play and stop the audio.
                 self.playing = QPushButton("Play The Audio")
                 self.playing.setMinimumHeight(270)
                 self.playing.clicked.connect(self.playaudio)
                 self.stop_playing = QPushButton("Stop Playing")
                 self.stop_playing.clicked.connect(self.stopplay)
                 return self.playing,self.stop_playing
-            else: ##unknown type
+            else: ##unknown type (some unknwon errors may occur, if this happen then set a spare content)
                 self.label = QLabel(" ")
                 self.scroll = QScrollArea()
                 self.scroll.setFixedHeight(300)
                 self.scroll.setWidget(self.label)
                 return self.scroll
+        #no file exists or other unknown errors occur, set a spare content.
         except Exception as e:
             print(e)
             self.label = QLabel(" ")
@@ -90,6 +101,8 @@ class MediaPlayer(QWidget):
             self.scroll.setWidget(self.label)
             return self.scroll ## no file exists?
 
+    #the function used to organize the flashcard, including adding the question description, and the file rendering
+    #flashnum is the question_id.
     def renderflashcard(self,flashnum,answer=False):
         try:
             f = open("./current/metadata.txt", "r", encoding="utf-8")
@@ -104,7 +117,7 @@ class MediaPlayer(QWidget):
             f.close()
         except:
             pass
-
+        #if is to show answer flashcard
         if answer:
             try:
                 desc = open("./current/flashcards/"+str(flashnum)+"_answer_des.txt")
@@ -113,9 +126,18 @@ class MediaPlayer(QWidget):
                 desc.close()
             except: ## no desc
                 pass
+            #call the renderfile() to render the content of the file
+
             output = self.renderfile(flashnum,True)
-            self.layout.addWidget(output)
+            try:
+                self.layout.addWidget(output)
+            except Exception as e:
+                print(e)
+                print(output)
+                for components in output:
+                    self.layout.addWidget(components)
             return self.layout
+        #if is to show question flashcard
         else:
             try:
                 desc = open("./current/flashcards/"+str(flashnum)+"_des.txt")
@@ -124,6 +146,7 @@ class MediaPlayer(QWidget):
                 desc.close()
             except: ## no desc
                 pass
+            # call the renderfile() to render the content of the file
             output = self.renderfile(flashnum)
             #print(output)
             try:
@@ -151,9 +174,4 @@ class MediaPlayer(QWidget):
 
     def stopplay(self):
             self.player.stop()
-            if __name__ == "__main__":
-                app = QApplication(sys.argv)
-                window = MediaPlayer(2)
-                window.show()
-                sys.exit(app.exec())
 
